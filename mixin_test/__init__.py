ab53c84117f552e7dff38f4715f1274b2767b75e
Eugene Molotov <molotov@it-projects.info>
2020-07-30 12:56:36 +0500

    Added selection field

--- mixin_test/__init__.py
+++ mixin_test/__init__.py
@@ -5,6 +5,7 @@ class TestMixin(models.AbstractModel):
     _name = 'test.mixin'
 
     test_field = fields.Boolean("Test field")
+    test_field_selection = fields.Selection("Test selection field")
 
 
 class TestModel(models.Model):
