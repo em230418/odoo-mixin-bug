3bc23d1131e0daad0074e11296d1478a0925c21f
Eugene Molotov <molotov@it-projects.info>
2020-07-30 13:02:29 +0500

    saved

--- mixin_test/__init__.py
+++ mixin_test/__init__.py
@@ -1,17 +1,5 @@
 from odoo import models, fields
 
 
-class TestMixin(models.AbstractModel):
-    _name = 'test.mixin'
-
-    test_field = fields.Boolean("Test field")
-    test_field_selection = fields.Selection([
-        ("yes", "Da"),
-        ("no", "Net"),
-    ])
-
-
 class TestModel(models.Model):
     _name = "test.model"
-
-    _inherit = 'test.mixin'
